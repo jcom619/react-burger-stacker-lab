f4a618229c49a0342b802721d69180621d4c92f4
Jcom619 <joshuacompton@my.smccd.edu>
2021-03-12 06:30:39 -0800

    josh

--- react-burger-stacker-lab/src/App.js
+++ react-burger-stacker-lab/src/App.js
@@ -1,23 +1,28 @@
 import logo from "./logo.svg";
-import "./App.css";
 
+import "./App.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div class="all">
+      <section class="advert">
+        <span>
+          <div class="center">
+            <img src={logo} className="path App-logo" alt="logo" />
+            <h1 class="title">Burger{`(stack_erz )`}</h1>
+          </div>
+        </span>
+      </section>
+      <br></br>
+      <section class="wrapper">
+        <div class="c-two">
+          <h1 class="list-name"> ingredients</h1>
+        </div>
 
-        <h1>
-          Burger <code> {`<stack/>`} </code>er?
-        </h1>
-      </header>
+        <div class="c-two">
+          <h1>Burger Stack</h1>
+        </div>
+      </section>
     </div>
-
-    //* <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"></a> */
   );
 }
 
